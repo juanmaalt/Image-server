b978466fd03c70a2ccbe54bca0850358133f57cc
juanmaalt <juanmaalt@gmail.com>
2021-03-29 21:53:47 -0300

    updating cors config

--- src/main/kotlin/com/juanmaalt/imageServer/config/CustomWebSecurityAdapter.kt
+++ src/main/kotlin/com/juanmaalt/imageServer/config/CustomWebSecurityAdapter.kt
@@ -1,16 +1,22 @@
 package com.juanmaalt.imageServer.config
 
 import com.juanmaalt.imageServer.security.APIKeyFilter
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class CustomWebSecurityAdapter : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity?) {
-        http?.sessionManagement()
+        http?.cors()
+            ?.and()
+            ?.sessionManagement()
             ?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             ?.and()
             ?.authorizeRequests()
@@ -20,8 +26,16 @@ class CustomWebSecurityAdapter : WebSecurityConfigurerAdapter() {
             ?.csrf()?.disable()
             ?.headers()?.disable()
             ?.httpBasic()
-            ?.and()
-            ?.cors()
         http?.addFilterAfter(APIKeyFilter(), BasicAuthenticationFilter::class.java)
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedMethods = listOf("GET", "POST")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
